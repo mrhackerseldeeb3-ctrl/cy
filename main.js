e906e503725b7e725cc8a5efb27490a121b4039d
Mr John wick <mrhackerseldeeb3@gmail.com>
2026-03-01 16:09:42 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+if (window.location.pathname.includes("index.html")) {
+
+  var name = window.prompt("اكتبي اسمك");
+
+  if (name) {
+    name = name.trim();
+    window.alert("أهلا بك يا " + name + " في مشروع مصر الرقمية");
+  }
+
+}
+
